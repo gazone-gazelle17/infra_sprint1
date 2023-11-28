74f7241e24bf2003455b6e62db11dbb7b58dd92e
aleksandrgasymov <gazykh@yandex.ru>
2023-11-28 10:52:39 +0000

    two requirements

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -2,10 +2,9 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-load_dotenv()
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 SECRET_KEY = os.getenv('TOKEN')
 
 DEBUG = False
